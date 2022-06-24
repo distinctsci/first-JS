c5c9a04fb330e75712d4dad357d02cf3b0966346
distinctsci <68077565+distinctsci@users.noreply.github.com>
2022-06-24 14:57:39 +0100

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+let myName = "Olaoluwa AJILEYE";
+const height = 5.7;
+var country = "Nigeria";
+
+document.write("My name is " + myName + "!<br>")
+document.write("\n")
+document.write("I am " + height + " ft tall!<br>")
+document.write("I am from " + country + "!")
